d28297ccbfddc3f35befabed11d7f316c9a99fe6
Mellow <mainamachariake@example.com>
2026-04-14 17:57:05 +0300

    updates

--- example/script/DeployRemitToken.s.sol
+++ example/script/DeployRemitToken.s.sol
@@ -2,15 +2,14 @@
 pragma solidity ^0.8.20;
 
 import {Script, console} from "forge-std/Script.sol";
-import {RemitX} from "../src/RemitToken.sol"; 
+import {RemitToken} from "../src/RemitToken.sol"; 
 
 contract DeployRemitToken is Script {
-    function run() external returns (RemitToken {
+    function run() external returns (RemitToken) {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         
         vm.startBroadcast(deployerPrivateKey);
         
-        // Adjust constructor arguments if your token requires them
         RemitToken rmxToken = new RemitToken(); 
         
         vm.stopBroadcast();
